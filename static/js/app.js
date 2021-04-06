6313d4cbc8a51d089d0c42d9b72d076e0d5ca7d3
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-04-06 14:41:59 -0400

    4-6-2021_2:41PMEST

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,7 @@ function unpack(rows, index) {
 
 
 d3.json("samples.json").then(function (data) {
-    var ids = unpack(data.metadata, "id");
+    let ids = unpack(data.metadata, "id");
     d3.select("#data")
         .selectAll("select")
         .data(ids)
@@ -25,35 +25,35 @@ d3.json("samples.json").then(function (data) {
 d3.selectAll("#data").on("change", updatePlotly);
 
 function updatePlotly() {
-    var dropdownMenu = d3.select("#data");
-    var dataset = dropdownMenu.property("value");
+    let dropdownMenu = d3.select("#data");
+    let dataset = dropdownMenu.property("value");
     console.log(dataset)
 
 
     d3.json("samples.json").then((data) => {
-        var person_id = data.samples.find(({ id }) => id === dataset);
+        let person_id = data.samples.find(({ id }) => id === dataset);
         console.log(person_id);
-        var otu_ids_sliced = person_id.otu_ids.slice(0, 10);
+        let otu_ids_sliced = person_id.otu_ids.slice(0, 10);
         console.log(otu_ids_sliced);
-        var values_sliced = person_id.sample_values.slice(0, 10);
+        let values_sliced = person_id.sample_values.slice(0, 10);
         console.log(values_sliced);
-        var labels_sliced = person_id.otu_labels.slice(0, 10);
+        let labels_sliced = person_id.otu_labels.slice(0, 10);
 
 
-        var y = otu_ids_sliced.map(function (a) { return "OTU ID " + a; });
-        var x = values_sliced.sort((a, b) => a - b);
-        var labels = labels_sliced;
+        let y = otu_ids_sliced.map(function (a) { return "OTU ID " + a; });
+        let x = values_sliced.sort((a, b) => a - b);
+        let labels = labels_sliced;
 
-        var trace1 = {
+        let trace1 = {
             x: x,
             y: y,
             type: "bar",
             text: labels,
             orientation: "h"
 };
-var data1 = [trace1];
+let data1 = [trace1];
 
-        var layout = {
+        let layout = {
             title: "Top 10 OTU",
             barmode: "group"
         };
